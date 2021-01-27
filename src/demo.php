f8e0ff7589bef8d6cc3eadc807378d9b0cb8dc94
valpfeil <valentin.pfeil@ecole-air.fr>
2021-01-27 09:39:53 +0100

    Hackzilla Password Generator

---
+++ src/demo.php
@@ -0,0 +1,20 @@
+<?php
+namespace valpfei\td2;
+
+/**
+ * Illustrer le fonctionnement de PHPCS
+ */
+class demo
+{
+    public $version =1;
+    /**
+     * Tester le correcteur de fonction
+     *
+     * @return nothing
+     */
+    public function hello()
+    {
+        echo "Bonjour";
+        echo PHP_EOL;
+    }
+}
